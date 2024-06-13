69a7b323c8779b1745c95233eed270e72cde2bd1
LukasMartini <60274604+LukasMartini@users.noreply.github.com>
2024-06-13 14:34:07 -0400

    Flask API skeleton introduced to fetch_hand.py.

--- backend/fetch_hand.py
+++ backend/fetch_hand.py
@@ -1,8 +1,12 @@
 from server import get_db_connection
 from convert_history import parse_hand_history
 import psycopg2
+from flask import Flask
+
+fh_app = Flask(__name__)
 
 # This function should not introduce changes to the database. No commits are made.
+@fh_app.route('/fetch_hand', methods=['GET'])
 def fetch_hand(hand_id: int, username: str, conn: psycopg2.connect) -> str:
     cursor = conn.cursor()
     with open ('backend/sql/fetch_hand_query_template.sql') as fetch_hand_query_template:
@@ -14,5 +18,6 @@ def fetch_hand(hand_id: int, username: str, conn: psycopg2.connect) -> str:
 
     return output
 
-print(fetch_hand(1, 'Ted', get_db_connection()))
+fh_app.run()
+print(fetch_hand(2524, 'CashMatteo', get_db_connection())) # For testing purposes.
 
